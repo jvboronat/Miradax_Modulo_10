50924c29a280474cd2ed418aaba6172c320b2cd2
Boro <jobodo@softin.com>
2020-11-17 15:28:58 +0100

    Commit inicial

---
+++ app/install.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+
+installButton.addEventListener('click', installPWA);
+
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent)
+
+function saveBeforeInstallPromptEvent(evt)
+{
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+}
+
+function installPWA(evt)
+{
+    deferredInstallPrompt.prompt();
+    evt.srcElement.setAttribute('hidden',true);
+    deferredInstallPrompt.userChoice.then((choice) => {
+        if (choice.outcome == "accepted")
+        {
+            console.log("aceptado")
+        }
+        else
+        {
+            console.log("No aceptado")
+        }
+
+        deferredInstallPrompt = null;
+    })
+}
+
+window.addEventListener('appinstalled', logAppinstalled);
+function logAppinstalled(evt)
+{
+    console.log("Installed");
+}
